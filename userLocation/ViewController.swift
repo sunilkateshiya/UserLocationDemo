b5f757ab0be01f1bae93bcc6a5ca729bdd668bcf
iFlame <iflame@Administrators-iMac.local>
2017-05-27 10:29:41 +0530

    Initial Commit

---
+++ userLocation/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  userLocation
+//
+//  Created by iFlame on 5/19/17.
+//  Copyright © 2017 iFlame. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var map: MKMapView!
+    
+    let manager = CLLocationManager()
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations[0]
+        
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        map.setRegion(region, animated: true)
+        
+        print(location.altitude)
+        print(location.speed)
+        
+        self.map.showsUserLocation = true
+    }
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        map.delegate = self as? MKMapViewDelegate
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+      
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+       
+    }
+
+
+}
+
